ee6ac76d7b77bdd210de2d8d74a376203ce57bd6
Sim <0doogadooga@gmail.com>
2024-02-08 22:45:29 +0900

    main 화면 퍼블리싱 완료

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -10,11 +10,38 @@ export default function MainPage() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post__list">post List</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, i) => (
+          <div key={i} className="post__box">
+            <Link to={`/posts/${i}`}>
+              <div className="post__profile-box">
+                <div className="post__profile-img"></div>
+                <div className="post__author-name">test@naver.com</div>
+                <div className="post__date">2023.6.13 금요일</div>
+              </div>
+              <div className="post__title">게시글{i}</div>
+              <div className="post__text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque
+                distinctio, laborum aliquid exercitationem nihil iure
+                consectetur quo eaque, tempora error itaque soluta. Eligendi
+                soluta distinctio assumenda rerum repudiandae ipsam aut.
+              </div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu1</div>
-        <div>Menu2</div>
-        <div>Menu3</div>
+        <Link to="/posts/create">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   );
